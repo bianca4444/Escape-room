ddbfb7bb89160cd52c6dc143fecd2a80542de7c4
Joita Fabian Gabriel <joitafabi@gmail.com>
2024-11-16 13:33:27 +0200

    Initial commit
    # Conflicts:
    #	README.md

---
+++ core/src/main/kotlin/com/github/escape_room/poo/Escape_Room.kt
@@ -0,0 +1,6 @@
+package com.github.escape_room.poo
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Escape_Room : ApplicationAdapter()
