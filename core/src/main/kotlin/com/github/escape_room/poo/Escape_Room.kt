064ec76d9ed2461fea0879453e53c7463eb7716a
Joita Fabian Gabriel <joitafabi@gmail.com>
2024-11-23 15:38:47 +0200

    vid2

--- core/src/main/kotlin/com/github/escape_room/poo/Escape_Room.kt
+++ core/src/main/kotlin/com/github/escape_room/poo/Escape_Room.kt
@@ -1,6 +1,19 @@
 package com.github.escape_room.poo
 
+import com.badlogic.gdx.Application
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.github.escape_room.poo.screen.GameScreen
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
-class Escape_Room : ApplicationAdapter()
+
+class Escape_Room : KtxGame<KtxScreen>() {
+
+    override fun create() {
+        Gdx.app.logLevel = Application.LOG_DEBUG
+        addScreen(GameScreen())
+        setScreen<GameScreen>()
+    }
+}
